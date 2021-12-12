8166228820db98845abe62a3000a43dedf464e6d
Tristan Dyer <atdyer@ncsu.edu>
2021-12-12 13:39:16 -0500

    Attempt static build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const withNextra = require('nextra')({
-  theme: './nextra-theme-sterling',
-  themeConfig: './theme.config.js',
+const withNextra = require("nextra")({
+  theme: "./nextra-theme-sterling",
+  themeConfig: "./theme.config.js",
   unstable_stork: false,
-  unstable_staticImage: true,
-})
+  unstable_staticImage: false,
+});
 module.exports = withNextra({
   reactStrictMode: true,
   async redirects() {
@@ -11,8 +11,8 @@ module.exports = withNextra({
       {
         source: "/discord{/}?",
         permanent: true,
-        destination: "https://discord.gg/EWD3cH7q"
-      }
-    ]
-  }
-})
+        destination: "https://discord.gg/EWD3cH7q",
+      },
+    ];
+  },
+});
